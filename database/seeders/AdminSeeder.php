8bbc9c4e49f73a922262aa137edeb45d36e2b73a
Jimmy Parker <jimmy.tdh@gmail.com>
2021-02-10 15:05:49 +0800

    user priv

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user_priv')
+            ->insert([
+                'user_id' => 1,
+                'syscode' => 'intranet',
+                'level' => 'admin'
+            ]);
+    }
+}
