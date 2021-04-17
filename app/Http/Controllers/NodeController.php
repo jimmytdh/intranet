ea98c44c6984836ad1e172feb9ffa2bdc9c1978f
Jimmy Parker <jimmy.tdh@gmail.com>
2021-04-18 06:47:05 +0800

    fix node creation

--- app/Http/Controllers/NodeController.php
+++ app/Http/Controllers/NodeController.php
@@ -25,15 +25,7 @@ class NodeController extends Controller
 
     public function create(Request $req,$id)
     {
-        if(!self::hasFile($id)){
-            $data= array(
-                'parent_id' => $id,
-                'title' => $req->title,
-                'type' => self::nodeType($id)
-            );
-            Node::create($data);
-            return 1;
-        }else if($req->type=='Main'){
+        if($req->type=='Main'){
             $parent_id = Node::find($id)->parent_id;
             $data = array(
                 'parent_id' => $parent_id,
@@ -42,7 +34,33 @@ class NodeController extends Controller
             );
             Node::create($data);
             return 1;
+        }else if(!self::hasFile($id)){
+            $data= array(
+                'parent_id' => $id,
+                'title' => $req->title,
+                'type' => self::nodeType($id)
+            );
+            Node::create($data);
+            return 1;
         }
+//        if(!self::hasFile($id)){
+//            $data= array(
+//                'parent_id' => $id,
+//                'title' => $req->title,
+//                'type' => self::nodeType($id)
+//            );
+//            Node::create($data);
+//            return 1;
+//        }else if($req->type=='Main'){
+//            $parent_id = Node::find($id)->parent_id;
+//            $data = array(
+//                'parent_id' => $parent_id,
+//                'title' => $req->title,
+//                'type' =>  ($parent_id==0) ? 'main': self::nodeType($parent_id)
+//            );
+//            Node::create($data);
+//            return 1;
+//        }
         return 0;
     }
 
